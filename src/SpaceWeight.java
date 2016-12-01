b75f3bea468210b26d52ecfb1f68e9028d485f88
1CameronT <camerontoyufc@outlook.com>
2016-12-01 15:54:37 -0600

    Lab 4-1
    finished space weight

---
+++ src/SpaceWeight.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author c.toy
+ *
+ */
+public class SpaceWeight {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double userWeight;
+		
+		Scanner userInput = new Scanner(System.in);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
